c0f1069b4ee6844ebdce6688c2cb4841da2cb69b
Chalith Liyanage <chalith92@gmail.com>
2026-09-20 17:07:18 +0300

    Implementation of assignment week4

--- backend/app.py
+++ backend/app.py
@@ -2,28 +2,45 @@ from flask import Flask, jsonify
 import os
 import mysql.connector
 
-app = Flask(___name___)
+app = Flask(__name__)
 
 DB_HOST = os.getenv('DB_HOST','db')
 DB_USER = os.getenv('DB_USER', 'appuser')
-DB_PASSWORD = os.getuser('DB-PASSWORD','changeme')
+DB_PASSWORD = os.getenv('DB_PASSWORD','changeme')
 DB_NAME = os.getenv('DB_NAME','appdb')
 
 @app.get('/api/health')
 def health():
-        return jsonify(status='ok')
+      return jsonify(status='ok')
 
-@app.get('/api')
+@app.route('/api')
 def index():
-    conn = mysql.connector.connect(
-          hist = DB_HOST, user =DB_USER, password = DB_PASSWORD, database = DB_NAME
-    )
-    cur = conn.cursor()
-    cur.execute("SELECT 'Hello from MYSQL via Flask! '")
-    row = cur.fetchone()
-    cur.close()
-    conn.close()
-    return jsonify(message = row[0])
+    try:
+      conn = mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
+      cur = conn.cursor()
 
+      cur.execute("SELECT 'Hello from MYSQL via Flask! '")
+      message = cur.fetchone()[0]
+
+      cur.execute("INSERT INTO counter () VALUES ()")
+      conn.commit()
+        
+      cur.execute("SELECT COUNT(*) FROM counter")
+      visitor_count = cur.fetchone()[0]
+
+      cur.execute("SELECT NOW()")
+      db_time = cur.fetchone()[0]
+        
+      cur.close()
+      conn.close()
+        
+      return jsonify({
+            "message": message,
+            "visitor_count": visitor_count,
+            "db_time": str(db_time)
+      })
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
 if __name__ == '__main__':
-      app.run(host = '0.0.0.0', port = 8000, debug = true)
+      app.run(host = '0.0.0.0', port = 8000, debug = True)
